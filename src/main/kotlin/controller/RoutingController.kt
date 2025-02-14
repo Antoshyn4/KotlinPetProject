3e7354fa69ae8385cc435b5a3882ad05b0dcc5eb
Dmytro <dimamar1003@gmail.com>
2025-02-15 01:24:17 +0200

    Added Routing

---
+++ src/main/kotlin/controller/RoutingController.kt
@@ -0,0 +1,16 @@
+package controller
+
+import io.ktor.http.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+
+fun Route.RoutingController() {
+    get("/api/v1/delivery-order-price") {
+        println("check")
+    }
+    get ("/*"){
+        call.response.status(HttpStatusCode(404, "Not found"))
+        call.respond("Bad Request, Not found");
+    }
+}
--- src/main/kotlin/plugins/Routing.kt
+++ src/main/kotlin/plugins/Routing.kt
@@ -3,9 +3,11 @@ package org.kotlin.ktor
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import controller.RoutingController
 
 fun Application.configureRouting() {
     routing {
+        RoutingController()
         get("/") {
             call.respondText("Hello World!")
         }
